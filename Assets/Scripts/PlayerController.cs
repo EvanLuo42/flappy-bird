0bb2f6afd38c6e8b5ed10e17d30fffb7a144b765
Evan Luo <knacks.insects_0o@icloud.com>
2024-03-27 14:36:28 +0800

    feat: Auto map generate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float scrollSpeed;
     public float maxSpeed;
+    public float duration;
+    public GameObject pipelinePrefab;
+
 
     private Rigidbody2D _rigidbody2D;
+    private readonly Queue<KeyValuePair<GameObject, GameObject>> _pipelines = new();
+    private float _time = -8;
 
     private void Start()
     {
+        var position = transform.position;
+        for (var i = 1; i < 6; i++)
+        {
+            var y = Random.Range(0, 10);
+            var top = Instantiate(
+                pipelinePrefab,
+                new Vector2(position.x + i * 10, y),
+                new Quaternion(0, 0, 0, 0));
+            var bottom = Instantiate(
+                pipelinePrefab,
+                new Vector2(position.x + i * 10, y - 10),
+                new Quaternion(0, 0, 0, 0));
+
+            _pipelines.Enqueue(new KeyValuePair<GameObject, GameObject>(top, bottom));
+        }
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _rigidbody2D.velocity = Vector2.right * new Vector2(scrollSpeed, 0);
     }
@@ -18,6 +40,15 @@ public class PlayerController : MonoBehaviour
         _rigidbody2D.velocity = new Vector2(
             x: _rigidbody2D.velocity.x,
             y: Input.GetAxis("Vertical") * maxSpeed);
+        _time += Time.deltaTime;
+        if (!(_time >= duration)) return;
+        var pipeline = _pipelines.Dequeue();
+        var x = _pipelines.Last().Key.transform.position.x + 10;
+        var y = Random.Range(0, 10);
+        pipeline.Key.transform.position = new Vector3(x, y);
+        pipeline.Value.transform.position = new Vector3(x, y - 10);
+        _pipelines.Enqueue(pipeline);
+        _time = 0;
     }
 
     private void OnCollisionEnter2D()
